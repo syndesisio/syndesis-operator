5ed2215385de434fe628ae3e814bcccf9290c9e9
nferraro <ni.ferraro@gmail.com>
2018-07-09 11:05:39 +0200

    Added some hacks for code generation

--- resources/bindata.go
+++ resources/bindata.go
@@ -83,7 +83,7 @@ func bindataGo() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "bindata.go", size: 0, mode: os.FileMode(436), modTime: time.Unix(1530700649, 0)}
+	info := bindataFileInfo{name: "bindata.go", size: 0, mode: os.FileMode(436), modTime: time.Unix(1531126567, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
